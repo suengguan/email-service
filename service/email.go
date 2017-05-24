5ad5dd1ea876175a7734c60b0d4bbe5205b290c0
suengguan <suengguan@126.com>
2017-05-24 10:39:38 +0800

    modify service file

--- service/email.go
+++ service/email.go
@@ -93,21 +93,21 @@ func (this *EmailService) sendMail(user, password, host, to, subject, body, mail
 	ch <- err
 }
 
-func (this *EmailService) SendEmail(user, password, host, to, subject, body, mailtype string) {
-	hp := strings.Split(host, ":")
-	auth := smtp.PlainAuth("", user, password, hp[0])
-	var content_type string
-	if mailtype == "html" {
-		content_type = "Content-Type: text/" + mailtype + "; charset=UTF-8"
-	} else {
-		content_type = "Content-Type: text/plain" + "; charset=UTF-8"
-	}
+// func (this *EmailService) SendEmail(user, password, host, to, subject, body, mailtype string) {
+// 	hp := strings.Split(host, ":")
+// 	auth := smtp.PlainAuth("", user, password, hp[0])
+// 	var content_type string
+// 	if mailtype == "html" {
+// 		content_type = "Content-Type: text/" + mailtype + "; charset=UTF-8"
+// 	} else {
+// 		content_type = "Content-Type: text/plain" + "; charset=UTF-8"
+// 	}
 
-	msg := []byte("To: " + to + "\r\nFrom: " + user + "<" + user + ">\r\nSubject: " + subject + "\r\n" + content_type + "\r\n\r\n" + body)
-	send_to := strings.Split(to, ";")
-	err := smtp.SendMail(host, auth, user, send_to, msg)
-	fmt.Println(err)
-}
+// 	msg := []byte("To: " + to + "\r\nFrom: " + user + "<" + user + ">\r\nSubject: " + subject + "\r\n" + content_type + "\r\n\r\n" + body)
+// 	send_to := strings.Split(to, ";")
+// 	err := smtp.SendMail(host, auth, user, send_to, msg)
+// 	fmt.Println(err)
+// }
 
 // func main() {
 // 	user := "suengguan@126.com"
